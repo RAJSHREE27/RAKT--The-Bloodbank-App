d4b6091512071a7d4b3705a474dd63367aab1d0e
RAJSHREE27 <rajshreegavel111@gmail.com>
2019-02-13 23:31:09 +0530

    Donations schema created

---
+++ db/donations/Donationmodel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const donationSchema = new mongoose.Schema({
+
+   user_id : {
+     type : mongoose.Schema.ObjectId,
+     ref : 'User',
+     required : true
+   },
+  bloodbank_id : {
+     type : mongoose.Schema.ObjectId,
+     ref : 'Bloodbank' ,
+     required  : true
+   } ,
+  timestamp :{
+     type : Date,
+     default : Date.now
+   },
+  quantity : {
+     type : Number,
+     default : 1,
+     minlength : 1,
+     maxlength : 1 ,
+     match : [/^\d+$/, 'Please fill a valid quantity']
+   }
+
+});
+
+const Donations = mongoose.model('Donations', donationSchema);
+module.exports = Donations
