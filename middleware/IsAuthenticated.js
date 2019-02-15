9ffb0c741478c8d33195c9425c24b06091de8479
RAJSHREE27 <rajshreegavel111@gmail.com>
2019-02-16 00:20:44 +0530

    user login token generated

---
+++ middleware/IsAuthenticated.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtokens');
+
+let verifyToken = (req, res, next) =>{
+   let token = req.headers['authorization'] || req.headers['x-access-token'];
+   if(token.startsWith('Bearer ')){
+     token = token.slice(7 , token.length);
+
+   }
+   if(token){
+     jwt.verify(token, process.env.SECRET , (err , decoded)=>{
+       if(err){
+         return res.json({
+           success : false,
+           message : " token is not valid "
+         });
+       }else{
+         req.decoded = decoded;
+         next();
+       }
+     });
+   }else{
+     return res.json({
+       success : false ,
+       message : " auth token is not supplied "
+     });
+   }
+
+};
+
+module.exports = verifyToken;
